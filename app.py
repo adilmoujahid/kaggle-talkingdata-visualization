9532d1b90c05cfdd1f738f14d54c1e6d260a2df5
Adil Moujahid <adil.mouja@gmail.com>
2016-07-22 01:27:36 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+from shapely.geometry import Point, shape
+
+from flask import Flask
+from flask import render_template
+import json
+
+
+data_path = '../input/'
+n_samples = 30000
+
+def get_age_segment(age):
+    if age <= 22:
+        return '22-'
+    elif age <= 26:
+        return '23-26'
+    elif age <= 28:
+        return '27-28'
+    elif age <= 32:
+        return '29-31'
+    elif age <= 38:
+        return '32-38'
+    else:
+        return '39+'
+
+def get_location(longitude, latitude, provinces_json):
+    
+    point = Point(longitude, latitude)
+
+    for record in provinces_json['features']:
+        polygon = shape(record['geometry'])
+        if polygon.contains(point):
+            return record['properties']['name']
+    return 'other'
+
+
+with open(data_path + '/geojson/china_provinces_en.json') as data_file:    
+    provinces_json = json.load(data_file)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/data")
+def get_data():
+    gen_age_tr = pd.read_csv(data_path + 'gender_age_train.csv')
+    ev = pd.read_csv(data_path + 'events.csv')
+    ph_br_dev_model = pd.read_csv(data_path + 'phone_brand_device_model.csv')
+
+    df = gen_age_tr.merge(ev, how='left', on='device_id')
+    df = df.merge(ph_br_dev_model, how='left', on='device_id')
+    #Get n_samples records
+    df = df[df['longitude'] != 0].sample(n=n_samples)
+
+
+    top_10_brands_en = {'华为':'Huawei', '小米':'Xiaomi', '三星':'Samsung', 'vivo':'vivo', 'OPPO':'OPPO',
+                        '魅族':'Meizu', '酷派':'Coolpad', '乐视':'LeEco', '联想':'Lenovo', 'HTC':'HTC'}
+
+    df['phone_brand_en'] = df['phone_brand'].apply(lambda phone_brand: top_10_brands_en[phone_brand] 
+                                                    if (phone_brand in top_10_brands_en) else 'Other')
+
+    df['age_segment'] = df['age'].apply(lambda age: get_age_segment(age))
+
+    df['location'] = df.apply(lambda row: get_location(row['longitude'], row['latitude'], provinces_json), axis=1)
+
+    cols_to_keep = ['timestamp', 'longitude', 'latitude', 'phone_brand_en', 'gender', 'age_segment', 'location']
+    df_clean = df[cols_to_keep].dropna()
+
+    return df_clean.to_json(orient='records')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
---
+++ static/js/graphs.js
@@ -0,0 +1,155 @@
+queue()
+    .defer(d3.json, "/data")
+    .await(makeGraphs);
+
+function makeGraphs(error, recordsJson) {
+	
+	//Clean data
+	var records = recordsJson;
+	var dateFormat = d3.time.format("%Y-%m-%d %H:%M:%S");
+	
+	records.forEach(function(d) {
+		d["timestamp"] = dateFormat.parse(d["timestamp"]);
+		d["timestamp"].setMinutes(0);
+		d["timestamp"].setSeconds(0);
+		d["longitude"] = +d["longitude"];
+		d["latitude"] = +d["latitude"];
+	});
+
+	//Create a Crossfilter instance
+	var ndx = crossfilter(records);
+
+	//Define Dimensions
+	var dateDim = ndx.dimension(function(d) { return d["timestamp"]; });
+	var genderDim = ndx.dimension(function(d) { return d["gender"]; });
+	var ageSegmentDim = ndx.dimension(function(d) { return d["age_segment"]; });
+	var phoneBrandDim = ndx.dimension(function(d) { return d["phone_brand_en"]; });
+	var locationdDim = ndx.dimension(function(d) { return d["location"]; });
+	var allDim = ndx.dimension(function(d) {return d;});
+
+
+	//Group Data
+	var numRecordsByDate = dateDim.group();
+	var genderGroup = genderDim.group();
+	var ageSegmentGroup = ageSegmentDim.group();
+	var phoneBrandGroup = phoneBrandDim.group();
+	var locationGroup = locationdDim.group();
+
+	var all = ndx.groupAll();
+
+
+	//Define values (to be used in charts)
+	var minDate = dateDim.bottom(1)[0]["timestamp"];
+	var maxDate = dateDim.top(1)[0]["timestamp"];
+
+
+    //Charts
+	var timeChart = dc.barChart("#time-chart");
+	var genderChart = dc.rowChart("#gender-row-chart");
+	var ageSegmentChart = dc.rowChart("#age-segment-row-chart");
+	var phoneBrandChart = dc.rowChart("#phone-brand-row-chart");
+	var locationChart = dc.rowChart("#location-row-chart");
+	var numberRecordsND = dc.numberDisplay("#number-records-nd");
+
+
+	numberRecordsND
+		.formatNumber(d3.format("d"))
+		.valueAccessor(function(d){return d; })
+		.group(all);
+
+
+	timeChart
+		.width(650)
+		.height(140)
+		.margins({top: 10, right: 50, bottom: 20, left: 20})
+		.dimension(dateDim)
+		.group(numRecordsByDate)
+		.transitionDuration(500)
+		.x(d3.time.scale().domain([minDate, maxDate]))
+		.elasticY(true)
+		.yAxis().ticks(4);
+
+	genderChart
+        .width(300)
+        .height(100)
+        .dimension(genderDim)
+        .group(genderGroup)
+        .ordering(function(d) { return -d.value })
+        .colors(['#6baed6'])
+        .elasticX(true)
+        .xAxis().ticks(4);
+
+	ageSegmentChart
+		.width(300)
+		.height(150)
+        .dimension(ageSegmentDim)
+        .group(ageSegmentGroup)
+        .colors(['#6baed6'])
+        .elasticX(true)
+        .labelOffsetY(10)
+        .xAxis().ticks(4);
+
+	phoneBrandChart
+		.width(300)
+		.height(310)
+        .dimension(phoneBrandDim)
+        .group(phoneBrandGroup)
+        .ordering(function(d) { return -d.value })
+        .colors(['#6baed6'])
+        .elasticX(true)
+        .xAxis().ticks(4);
+
+    locationChart
+    	.width(200)
+		.height(510)
+        .dimension(locationdDim)
+        .group(locationGroup)
+        .ordering(function(d) { return -d.value })
+        .colors(['#6baed6'])
+        .elasticX(true)
+        .labelOffsetY(10)
+        .xAxis().ticks(4);
+
+    var map = L.map('map');
+
+	var drawMap = function(){
+
+	    map.setView([31.75, 110], 4);
+		mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+		L.tileLayer(
+			'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; ' + mapLink + ' Contributors',
+				maxZoom: 15,
+			}).addTo(map);
+
+		//HeatMap
+		var geoData = [];
+		_.each(allDim.top(Infinity), function (d) {
+			geoData.push([d["latitude"], d["longitude"], 1]);
+	      });
+		var heat = L.heatLayer(geoData,{
+			radius: 10,
+			blur: 20, 
+			maxZoom: 1,
+		}).addTo(map);
+
+	};
+
+	//Draw Map
+	drawMap();
+
+	//Update the heatmap if any dc chart get filtered
+	dcCharts = [timeChart, genderChart, ageSegmentChart, phoneBrandChart, locationChart];
+
+	_.each(dcCharts, function (dcChart) {
+		dcChart.on("filtered", function (chart, filter) {
+			map.eachLayer(function (layer) {
+				map.removeLayer(layer)
+			}); 
+			drawMap();
+		});
+	});
+
+	dc.renderAll();
+
+};
